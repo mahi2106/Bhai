a5c55d4b6466c1f7cb528eda04ba854b4a98486e
mahi2106 <129710437+mahi2106@users.noreply.github.com>
2023-06-12 13:07:34 +0530

    Create Pruth.java

---
+++ Mahi/src/test/java/com/Mahi/testscripts/Pruth.java
@@ -0,0 +1,11 @@
+package com.Mahi.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Pruth {
+	@Test
+public void testCreateCustomer() {
+	Reporter.log("Hi Maga",true);
+}
+}
